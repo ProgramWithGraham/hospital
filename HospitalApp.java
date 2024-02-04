66d91603e46516ca5c8130e2a795b1a7fe16b393
ProgramWithGraham <gaps512@gmail.com>
2024-02-04 14:01:50 -0600

    added Mcdreamy in HospitalApp

--- HospitalApp.java
+++ HospitalApp.java
@@ -1,6 +1,10 @@
 public class HospitalApp {
 
     public static void main(String[] args) {
+        Doctor mcDreamy = new Doctor(90000.00, "Derek Shepherd", 1, "Neurology");
+        System.out.println(mcDreamy);
 
+        int mcDreamyEmployeeNum = mcDreamy.getEmployeeNumber();
+        System.out.println("McDreamy's employee number is " + mcDreamyEmployeeNum);
     }
 }
